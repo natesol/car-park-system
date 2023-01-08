0b61ff696faacc316506683e38d9c62f2dc156eb
natesol <56076205+natesol@users.noreply.github.com>
2023-01-08 05:20:33 +0200

    Delete entities directory

--- entities/src/main/java/net/cps/entities/hibernate/OccasionalCustumer.java
+++
@@ -1,9 +0,0 @@
-package net.cps.entities.hibernate;
-
-import java.util.Date;
-
-public class OccasionalCustumer extends AbstractCostumer {
-    private Reservation reservation;
-    private Date timeOfDeparture;
-    private String email;
-}
